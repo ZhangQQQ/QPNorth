92cb19141b1a99a7d50133147cc1804dfebbfd82
1 <a1@1deMacBook-Pro.local>
2016-04-27 16:18:14 +0800

    初始化项目

---
+++ seccess/seccess/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  seccess
+//
+//  Created by 1 on 16/4/27.
+//  Copyright © 2016年 1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ seccess/seccess/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  seccess
+//
+//  Created by 1 on 16/4/27.
+//  Copyright © 2016年 1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
